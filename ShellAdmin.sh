d60d08a3c03772bd91c2a99df1e82292b9d91a57
Camila Quintero <71272040+camilaq98@users.noreply.github.com>
2023-03-07 11:38:41 -0500

    Update ShellAdmin.sh

--- ShellAdmin.sh
+++ ShellAdmin.sh
@@ -22,9 +22,7 @@ do           # Limpiar pantalla
 	echo "-  6. Editar DNS Server                -"   	
 	echo "- ------------------------------------ -" 
 	echo "-  7. Configurar proxy                 -"   	
-	echo "- ------------------------------------ -"  
-	echo "-  8. Instalar Docker                  -"   	
-	echo "- ------------------------------------ -"      
+	echo "- ------------------------------------ -"     
 	echo "-  E. Exit                             -"        
 	echo "- ------------------------------------ -"    # get input from the user         
 	read -p "Digite la opción [1-7] " choice
@@ -318,97 +316,6 @@ do           # Limpiar pantalla
 			echo -e "\n----- Fin del Script -----------------------------------------------------------"            
 			read -p "Press [Enter] key to continue..." readEnterKey
             ;;
-        8)
-            # Ejecucion
-            echo "-----------------------------------------------------------------------------"
-            echo "Inicia instalacion docker CE                                                 "
-            echo "-----------------------------------------------------------------------------"
-            read -p ">> Paso 1: Desea Instalar Docker (y/n)? " answer
-
-            if [[ $answer =~ ^[Yy]$ ]]
-                then
-                    cd ~/
-
-                    echo "-----------------------------------------------------------------------------"
-                    echo "Instalación Prerequisitios"
-                    echo "-----------------------------------------------------------------------------"
-                    sudo apt-get update -y
-                    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-                    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-                    sudo apt update -y
-                    apt-cache policy docker-ce -y
-                    sudo apt install docker-ce -y
-
-                    echo "-----------------------------------------------------------------------------"
-                    echo "Verificar Version"
-                    echo "-----------------------------------------------------------------------------"
-                    docker --version
-
-                    echo "-----------------------------------------------------------------------------"
-                    echo "Iniciar docker con el sistema"
-                    echo "-----------------------------------------------------------------------------"
-                    sudo systemctl enable docker
-                    sudo systemctl start docker
-                    
-                    echo "-----------------------------------------------------------------------------"
-                    echo "Crear usuario de Docker"
-                    echo "-----------------------------------------------------------------------------"
-                    sudo adduser docker
-
-                    
-                    echo "-----------------------------------------------------------------------------"
-                    echo "Agregar permisos usuario ubunutu al grupo Docker"
-                    echo "-----------------------------------------------------------------------------"
-                    user=$(whoami)
-                    sudo usermod -G docker $user
-                    grep $user /etc/group
-
-                    echo "-----------------------------------------------------------------------------"
-                    echo "folder docker"
-                    echo "-----------------------------------------------------------------------------"
-                    folder=/Images
-                    sudo mkdir -p $folder/$user
-                    sudo mkdir -p $folder/$user/Data
-                    sudo chown -R $user:$user $folder/$user
-                    sudo chown -R $user:$user $folder/$user/Data
-                    ls -ltr $folder/
-
-                    read -p "Press [Enter] key to continue..." readEnterKey
-
-                    echo "-----------------------------------------------------------------------------"
-                    echo "Inicia instalacion Docker Compose                                            "
-                    echo "-----------------------------------------------------------------------------"
-
-                    sudo mkdir -p /usr/local/bin
-                    sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-                    sudo chmod +x /usr/local/bin/docker-compose
-
-                    echo "-----------------------------------------------------------------------------"
-                    echo "Verificar docker-compose"
-                    echo "-----------------------------------------------------------------------------"
-                    sudo docker-compose --version
-
-                    read -p "Press [Enter] key to continue..." readEnterKey
-
-
-                    echo "-----------------------------------------------------------------------------"
-                    echo "Fin instalacion Docker                                                       "
-                    echo "-----------------------------------------------------------------------------"
-
-                    read -p "Press [Enter] key to continue..." readEnterKey
-            fi
-            echo "-----------------------------------------------------------------------------"
-            echo "Sin Ajustes!!"
-            echo "-----------------------------------------------------------------------------"
-                
-            echo ---------- Fin del Script ----------------------------
-            read -p "Press [Enter] key to continue..." readEnterKey
-            ;;
-		E)  
-            exit
-			;;
         *)
                 echo "Error: Invalid option..."                
 				read -p "Press [Enter] key to continue..." 
